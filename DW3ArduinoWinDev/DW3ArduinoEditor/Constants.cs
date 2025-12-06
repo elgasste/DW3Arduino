966cdfe8d4005c2405fbbedda3e46a2ddd457cdc
selgas <s.elgas@techsmith.com>
2025-12-06 16:15:28 -0500

    Try to load editor save data from JSON file

---
+++ DW3ArduinoWinDev/DW3ArduinoEditor/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace DW3ArduinoEditor
+{
+   public static class Constants
+   {
+      public const string AssetsBasePath = "..\\..\\..\\assets\\";
+      public const string OutputBasePath = "..\\..\\..\\..\\..\\DW3Arduino\\";
+
+      public const string SaveDataFileName = "EditorData.json";
+   }
+}
--- DW3ArduinoWinDev/DW3ArduinoEditor/ViewModels/MainWindowViewModel.cs
+++ DW3ArduinoWinDev/DW3ArduinoEditor/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,31 @@
 ﻿using DW3ArduinoEditor.SaveData;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
 
 namespace DW3ArduinoEditor.ViewModels
 {
    public class MainWindowViewModel : ViewModelBase
    {
       private readonly GameSaveData? _gameSaveData;
+
+      public MainWindowViewModel()
+      {
+         try
+         {
+            var contents = File.ReadAllText( Constants.AssetsBasePath + Constants.SaveDataFileName );
+            _gameSaveData = JsonSerializer.Deserialize<GameSaveData>( contents );
+         }
+         catch
+         {
+            _gameSaveData = null;
+         }
+
+         if ( _gameSaveData is null )
+         {
+            MessageBox.Show( "Failed to load save data from file! Creating new save data...", "Error", MessageBoxButton.OK, MessageBoxImage.Error );
+            _gameSaveData = new();
+         }
+      }
    }
 }
