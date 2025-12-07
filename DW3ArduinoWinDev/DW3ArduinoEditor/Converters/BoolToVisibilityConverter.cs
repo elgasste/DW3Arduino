179a130ce70ebef306c20bbbeb441d597f23c4e6
selgas <s.elgas@techsmith.com>
2025-12-07 16:40:39 -0500

    Added BoolToVisibilityConverter

---
+++ DW3ArduinoWinDev/DW3ArduinoEditor/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DW3ArduinoEditor.Converters
+{
+   public class BoolToVisibilityConverter : IValueConverter
+   {
+      public BoolToVisibilityConverter()
+      {
+         TrueValue = Visibility.Visible;
+         FalseValue = Visibility.Collapsed;
+      }
+
+      public Visibility TrueValue { get; set; }
+      public Visibility FalseValue { get; set; }
+
+      public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+      {
+         var val = System.Convert.ToBoolean( value );
+         return val ? TrueValue : FalseValue;
+      }
+
+      public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture ) => TrueValue.Equals( value );
+   }
+}
