2981deb2ce8f5a7f0fd6d4fe6192d346526d838a
Steve Elgas <elgasste@gmail.com>
2026-01-10 11:53:14 -0500

    Added player class attribute

--- DW3Arduino/player.h
+++ DW3Arduino/player.h
@@ -7,8 +7,25 @@
 
 typedef struct Entity_t Entity_t;
 
+typedef enum PlayerClass_t
+{
+   PlayerClass_Hero = 0,
+   PlayerClass_Soldier,
+   PlayerClass_Pilgrim,
+   PlayerClass_Wizard,
+   PlayerClass_Merchant,
+   PlayerClass_Fighter,
+   PlayerClass_Thief,
+   PlayerClass_GoofOff,
+   PlayerClass_Sage,
+
+   PlayerClass_Count
+}
+PlayerClass_t;
+
 typedef struct Player_t
 {
+   PlayerClass_t class;
    Entity_t* entity;
    u32 tileIndex;
 }
