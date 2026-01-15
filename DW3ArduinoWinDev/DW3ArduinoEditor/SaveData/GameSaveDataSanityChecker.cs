b6f3a0a1d7650029765e4c2ebf5cadd49fed1a8d
Steve Elgas <elgasste@gmail.com>
2026-01-15 17:14:03 -0500

    Sanity check header GUIDs

--- DW3ArduinoWinDev/DW3ArduinoEditor/SaveData/GameSaveDataSanityChecker.cs
+++ DW3ArduinoWinDev/DW3ArduinoEditor/SaveData/GameSaveDataSanityChecker.cs
@@ -8,26 +8,11 @@
                                       int activeSpriteTexturePoolCount,
                                       int playerSpriteTexturePoolCount )
       {
-         // tile texture sets
-         foreach ( var tileTextureSet in saveData.TileTextureSets )
-         {
-            if ( tileTextureSet.TileTexturePoolIndexes.Count >= Constants.TileTextureSetSize )
-            {
-               throw new Exception( string.Format( "Tile texture set \"{0}\" contains too many texture pool indexes", tileTextureSet.Name ) );
-            }
-
-            foreach ( var index in tileTextureSet.TileTexturePoolIndexes )
-            {
-               if ( index < 0 || index >= tileTexturePoolCount )
-               {
-                  throw new Exception( string.Format( "Tile texture set \"{0}\" contains an invalid texture pool index", tileTextureSet.Name ) );
-               }
-            }
-         }
+         CheckHeaderGuids( saveData.HeaderGuids );
+         CheckTileTextureSets( saveData.TileTextureSets, tileTexturePoolCount );
 
          // TODO
          //
-         // - check header guids, nothing should match and nothing should be empty
          // - make sure there aren't too many textures in each static sprite texture set
          // - make sure there aren't too many textures in each active sprite texture set
          // - make sure the number of player sprite textures matches the number of player classes
@@ -45,6 +30,55 @@
          // - make sure each tile map's NPCs are valid
          //    - no more than the maximum amount
          //    - entity index should be valid
+      }
+
+      private static void CheckHeaderGuids( HeaderGuidsSaveData saveData )
+      {
+         List<string> guids = [
+            saveData.TextBitFieldsHeaderGuid,
+            saveData.TileTexturesHeaderGuid,
+            saveData.StaticSpriteTexturesHeaderGuid,
+            saveData.ActiveSpriteTexturesHeaderGuid,
+            saveData.PlayerSpriteTexturesHeaderGuid,
+            saveData.MapTilesHeaderGuid
+         ];
+
+         for ( int i = 0; i < guids.Count; i++ )
+         {
+            if ( string.IsNullOrEmpty( guids[i] ) )
+            {
+               throw new Exception( "Header GUID is empty" );
+            }
+            else
+            {
+               for ( int j = 0; j < guids.Count; j++ )
+               {
+                  if ( i != j && guids[i] == guids[j] )
+                  {
+                     throw new Exception( "Header GUIDs are identical" );
+                  }
+               }
+            }
+         }
+      }
+
+      private static void CheckTileTextureSets( List<TileTextureSetSaveData> tileTextureSets, int tileTexturePoolCount )
+      {
+         foreach ( var tileTextureSet in tileTextureSets )
+         {
+            if ( tileTextureSet.TileTexturePoolIndexes.Count >= Constants.TileTextureSetSize )
+            {
+               throw new Exception( string.Format( "Tile texture set \"{0}\" contains too many texture pool indexes", tileTextureSet.Name ) );
+            }
+
+            foreach ( var index in tileTextureSet.TileTexturePoolIndexes )
+            {
+               if ( index < 0 || index >= tileTexturePoolCount )
+               {
+                  throw new Exception( string.Format( "Tile texture set \"{0}\" contains an invalid texture pool index", tileTextureSet.Name ) );
+               }
+            }
+         }
       }
    }
 }
