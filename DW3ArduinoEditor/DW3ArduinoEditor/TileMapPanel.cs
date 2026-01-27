edf8c85f080531d59cb6fb3c13c1628dcc0fb738
Steve Elgas <elgasste@gmail.com>
2026-01-27 09:00:14 -0500

    Removed unnecessary using

--- DW3ArduinoEditor/DW3ArduinoEditor/TileMapPanel.cs
+++ DW3ArduinoEditor/DW3ArduinoEditor/TileMapPanel.cs
@@ -3,7 +3,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
 using DW3ArduinoEditor.Enums;
 using DW3ArduinoEditor.Graphics;
 using DW3ArduinoEditor.ViewModels;
